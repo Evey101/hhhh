91a19c1ab917ed48f1c788c2a8271a7d95f53d91
Evey101 <eveylareyna@gmail.com>
2020-01-10 12:24:11 -0500

    init commit

---
+++ testing/Assets/script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class script : MonoBehaviour
+{
+    public string[] phrases;
+    public int num;
+    public bool mode;
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mode = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode == true)
+        {
+            text.text = phrases[Random.Range(0, 9)];
+            mode = false;
+        }
+    }
+
+    public void ButtonPressed()
+    {
+        mode = true;
+    }
+}
